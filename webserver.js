08513250ea350162acc8647fbe2ec18b14cbc38f
rkrahul523 <57534781+rkrahul523@users.noreply.github.com>
2025-12-25 19:09:45 +0530

    webserver updated

--- webserver.js
+++ webserver.js
@@ -1,15 +1,15 @@
 //Install express server
 const express = require('express');
 const path = require('path');
- 
+
 const app = express();
  
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/SuvidhaApp/browser'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/SuvidhaApp/browser/index.csr.html'));
+// Replace the static line with:
+app.use(express.static('./dist/SuvidhaApp'));
+app.get('/', (req, res) => {
+    console.log('Registering route1:', '/your/path/here');
+    res.sendFile(path.join(__dirname, 'dist/SuvidhaApp/browser/index.csr.html'));
 });
  
 // Start the app by listening on the default Heroku port
