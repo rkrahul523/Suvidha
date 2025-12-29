db901da68803317067f841186f6bdd0a9b480b28
rkrahul523 <57534781+rkrahul523@users.noreply.github.com>
2025-12-29 17:18:19 +0530

    api address updated

--- src/app/services/time-api-service.ts
+++ src/app/services/time-api-service.ts
@@ -19,9 +19,9 @@ export class TimeTableApiService {
   private timeapiUrl = 'https://api.ipgeolocation.io/ipgeo?apiKey=c44d4e73d2e44e0e9c57a3959f9683b6'; // API for IST time
 
     // apiURL = !window.location.origin.includes('localhost') ?
-      // apiURL=  'https://big-basket-tracker.onrender.com/'
+      apiURL=  'https://big-basket-tracker.onrender.com/'
     //     :
-    apiURL=  'http://localhost:5000/';
+    // apiURL=  'http://localhost:5000/';
 
     public leaveManagerData= signal<Leave[]>([
       { id: 1, day:0.5, employee: 'John Doe', type: 'HPL', fromDate: '26-12-2025', toDate: '26-12-2025' , leaveId: "1262666"},
