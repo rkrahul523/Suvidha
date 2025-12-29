fadec9d92f9c7796a6748adb8fc50b2a11705741
rkrahul523 <57534781+rkrahul523@users.noreply.github.com>
2025-12-29 18:34:21 +0530

    error event removed

--- src/app/services/error-catching.interceptor.ts
+++ src/app/services/error-catching.interceptor.ts
@@ -2,11 +2,10 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
-import { ToastrService } from 'ngx-toastr';
 import { TimeTableApiService } from './time-api-service';
 
 export const errorCatchingInterceptor: HttpInterceptorFn = (req, next) => {
-  const toastr = inject(ToastrService);
+  // const toastr = inject(ToastrService);
   const authentication = inject(TimeTableApiService);
 
   return next(req).pipe(
@@ -17,15 +16,15 @@ export const errorCatchingInterceptor: HttpInterceptorFn = (req, next) => {
       }
       
       let errorMsg = '';
-      if (error.error instanceof ErrorEvent) {
-        console.log('This is client side error');
-        errorMsg = `Error: ${error.error.message}`;
-      } else {
-        console.log('This is server side error');
-        errorMsg = `Error Code: ${error.status}, Message: ${error.message}`;
-      }
-      
-      toastr.error('Please try Again', 'Something went wrong!!!', { timeOut: 3000 });
+      // if (error.error instanceof ErrorEvent) {
+      //   console.log('This is client side error');
+      //   errorMsg = `Error: ${error.error.message}`;
+      // } else {
+      //   console.log('This is server side error');
+      //   errorMsg = `Error Code: ${error.status}, Message: ${error.message}`;
+      // }
+      authentication.errorToast('Please try Again', 'Something went wrong!!!')
+      // toastr.error('Please try Again', 'Something went wrong!!!', { timeOut: 3000 });
       return throwError(() => errorMsg);
     })
   );
